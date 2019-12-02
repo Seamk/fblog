1a7fb2fff613f099772338c755df5ad8f07dc7fd
fulei <fulei@yunjiacloud.com>
2019-12-02 18:37:10 +0800

    添加websocket

---
+++ src/main/java/com/fulei/fblog/config/WebSocketConfig.java
@@ -0,0 +1,12 @@
+package com.fulei.fblog.config;
+
+/**
+ * @author fulei
+ * @Title: WebSocketConfig
+ * @ProjectName fblog
+ * @Description: WebSocketConfig
+ * @date 2019/12/2 18:31
+ */
+public class WebSocketConfig {
+
+}
