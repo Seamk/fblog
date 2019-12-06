f9c4141bf51bb337742e80fea8ad341a063cc02a
fulei <fulei@yunjiacloud.com>
2019-12-06 18:36:38 +0800

    spring data rest

--- src/main/java/com/fulei/fblog/FblogApplication.java
+++ src/main/java/com/fulei/fblog/FblogApplication.java
@@ -3,6 +3,7 @@ package com.fulei.fblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,7 +15,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @date 2019/11/14 17:16
  */
 @SpringBootApplication
-@ComponentScan(value = {"com.fulei.fblog.controller","com.fulei.fblog.service.impl.jpa","com.fulei.fblog.config.stomp"})
+@ServletComponentScan(value = {"com.fulei.fblog.controller","com.fulei.fblog.servlet"})
+@ComponentScan(value = {"com.fulei.fblog.service.impl.jpa","com.fulei.fblog.config.stomp","com.fulei.fblog.repository"})
 @EnableJpaRepositories("com.fulei.fblog.repository")
 @EntityScan("com.fulei.fblog.domain")
 public class FblogApplication {
--- src/main/java/com/fulei/fblog/repository/UserInfoRepository.java
+++ src/main/java/com/fulei/fblog/repository/UserInfoRepository.java
@@ -3,6 +3,8 @@ package com.fulei.fblog.repository;
 
 import com.fulei.fblog.domain.UserInfoDo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author fulei
@@ -11,6 +13,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @Description: UserInfoRepository
  * @date 2019/11/5 16:56
  */
+@RepositoryRestResource(path="user")
+@Repository
 public interface UserInfoRepository extends JpaRepository<UserInfoDo,String> {
 
   UserInfoDo findByUserName(String userName);
